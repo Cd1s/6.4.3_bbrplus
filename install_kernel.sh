9f2ed46a01eb7ab69f917b15f06a91247b53de66
Cd1s <135119404+Cd1s@users.noreply.github.com>
2023-09-14 05:39:17 +0800

    Update install_kernel.sh

--- install_kernel.sh
+++ install_kernel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Download kernel header and image deb packages
-wget https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/6.4.3-bbrplus/Debian-Ubuntu_Optional_linux-headers-6.4.3-bbrplus_6.4.3-1_amd64.deb
-wget https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/6.4.3-bbrplus/Debian-Ubuntu_Required_linux-image-6.4.3-bbrplus_6.4.3-1_amd64.deb
+wget https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/6.5.2-bbrplus/Debian-Ubuntu_Optional_linux-headers-6.5.2-bbrplus_6.5.2-1_amd64.deb
+wget https://github.com/UJX6N/bbrplus-6.x_stable/releases/download/6.5.2-bbrplus/Debian-Ubuntu_Required_linux-image-6.5.2-bbrplus_6.5.2-1_amd64.deb
 
 # Install kernel header and image packages
-sudo dpkg -i Debian-Ubuntu_Optional_linux-headers-6.4.3-bbrplus_6.4.3-1_amd64.deb
-sudo dpkg -i Debian-Ubuntu_Required_linux-image-6.4.3-bbrplus_6.4.3-1_amd64.deb
+sudo dpkg -i Debian-Ubuntu_Optional_linux-headers-6.5.2-bbrplus_6.5.2-1_amd64.deb
+sudo dpkg -i Debian-Ubuntu_Required_linux-image-6.5.2-bbrplus_6.5.2-1_amd64.deb
 
 # Install dependencies if needed
 sudo apt install -f
